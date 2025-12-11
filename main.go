a413547230fdecab173f70513cbf332eb5412db7
Elena <elenatur03@outlook.com>
2025-12-11 16:32:47 +0300

    cr func max num

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) ([]int, error) {
 	if size <= 0 {
-		return nil, errors.New("размер массива должен быть положительным")
+		return nil, errors.New("размер должен быть положительным")
 	}
-	if size > SIZE {
-		return nil, errors.New("размер массива больше максимально допустимого")
+	if size > 100_000_000 {
+		return nil, errors.New("размер больше максимально допустимого")
 	}
 
 	src := rand.NewSource(time.Now().Unix())
@@ -33,7 +33,18 @@ func generateRandomElements(size int) ([]int, error) {
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) (int, error) {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0, errors.New("передан пустой слайс")
+	}
+
+	maxValue := data[0]
+	for i := range len(data) {
+		if data[i] > maxValue {
+			maxValue = data[i]
+		}
+	}
+
+	return maxValue, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
@@ -44,13 +55,22 @@ func maxChunks(data []int) (int, error) {
 func main() {
 	fmt.Printf("Генерируем %d целых чисел", SIZE)
 	data, err := generateRandomElements(SIZE)
+
 	if err != nil {
 		fmt.Printf("Ошибка генерации: %v\n", err)
+		return
 	}
 
+	// Однопоточный поиск
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	start := time.Now()
+	max, err := maximum(data)
 
+	if err != nil {
+		fmt.Printf("Ошибка поиска максимума: %v\n", err)
+		return
+	}
+	elapsed := time.Since(start).Microseconds() //?
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
